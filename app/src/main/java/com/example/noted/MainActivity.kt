c57ad5b87316f8d6c0c34cf26891d86e815f12ff
Administrator <godxuche@gmail.com>
2021-10-23 07:31:17 +0100

    start the NewWordActivity for result to get the input word...

--- app/src/main/java/com/example/noted/MainActivity.kt
+++ app/src/main/java/com/example/noted/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.noted
 
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.lifecycle.Observer
@@ -41,17 +46,41 @@ class MainActivity : AppCompatActivity() {
             words?.let { adapter.submitList(it) }
         })
 
+        val intent = Intent(this, NewWordActivity::class.java)
         binding.fab.setOnClickListener {
-            getContent.launch("string")
+            getContent.launch(intent)
         }
 
     }
-    val getContent = registerForActivityResult(ActivityResultContracts.GetContent(),) {uri: Uri? ->
+    // Registering the contract to the Activity
+    private val getContent = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        // The parseResult will return this as String?
+        if (result.resultCode == Activity.RESULT_OK) {
+            val data = result.data //.getStringExtra(NewWordActivity.EXTRA_REPLY)
+            val resultExtra = data?.getStringExtra(NewWordActivity.EXTRA_REPLY)
+            Toast.makeText(this, "Result: $resultExtra", Toast.LENGTH_LONG).show()
+        }
+        else
+            Toast.makeText(this, "No Result", Toast.LENGTH_SHORT).show()
 
+        // For custom Contracts
+//        if (result != null) //
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
+}
 
+class NewWordActivityContract: ActivityResultContract<Int, String?>() {
+    override fun createIntent(context: Context, input: Int?): Intent {
+        return Intent(context, NewWordActivity::class.java).apply {
+            putExtra("my_input_key", input)
+        }
+    }
+
+    override fun parseResult(resultCode: Int, intent: Intent?): String? {
+        val data = intent?.getStringExtra(NewWordActivity.EXTRA_REPLY)
+        return if (resultCode == Activity.RESULT_OK && data != null)
+            data
+        else
+            null
     }
 }
